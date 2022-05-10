c0838046c6c221f1b0d25ff9264aff530df32106
Gruy <mickael.gerard.dev@gmail.com>
2022-05-10 21:22:34 +0200

    create class ExperiencePro

--- app_site_cv/experience/models.py
+++ app_site_cv/experience/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class ExperiencePro(models.Model):
+    titre = models.CharField(max_length=150)
+    contenu = models.TextField()
+    slug = models.SlugField(max_length=100)
+    date_start = models.DateTimeField()
+    date_end = models.DateTimeField()
+    image = models.ImageField(default='default.jpg')
+
+    # pour avoir le Titre sur la page
+    def __str__(self):
+        return self.titre
