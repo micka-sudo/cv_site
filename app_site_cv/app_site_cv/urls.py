b30796d0a4e91324a8fd993c780836b7ec6bf282
Gruy <mickael.gerard.dev@gmail.com>
2022-05-10 14:47:18 +0200

    make home first page in path

--- app_site_cv/app_site_cv/urls.py
+++ app_site_cv/app_site_cv/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('home/', include('home.urls')),
+    path('', include('home.urls'), name='index'),
     path('admin/', admin.site.urls),
 ]
