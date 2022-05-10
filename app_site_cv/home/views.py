b3bea9a4f08b20624a5fd7345217de400d11a46e
Gruy <mickael.gerard.dev@gmail.com>
2022-05-10 14:48:00 +0200

    add render

--- app_site_cv/home/views.py
+++ app_site_cv/home/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
