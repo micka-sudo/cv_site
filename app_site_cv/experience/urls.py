c0d5e07cbe99b52ca1e1141a7dc405727d6b6c5b
Gruy <mickael.gerard.dev@gmail.com>
2022-05-10 22:43:47 +0200

    add app_name

--- app_site_cv/experience/urls.py
+++ app_site_cv/experience/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "experiences"
+
 urlpatterns = [
-    path('experience', views.experience_pro, name='experience'),
+    path('experience', views.experience_pro, name='experiences'),
     path('<slug:slug>', views.experience_pro, name='experience')
 ]
