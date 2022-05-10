ff498a7d176cc1544147fa3b6295a99e2d550c05
Gruy <mickael.gerard.dev@gmail.com>
2022-05-10 21:35:17 +0200

    import

--- app_site_cv/experience/views.py
+++ app_site_cv/experience/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import ExperiencePro
+from django.http import Http404, HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
+from .forms import ExperienceForm
+from django.urls import reverse
 
 
 def experience_pro(request):
+    experiences = ExperiencePro.objects.all()
     return render(request, 'experience/experience.html')
