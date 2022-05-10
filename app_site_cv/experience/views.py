03709abfdd35da62226f69ca2acc3877989c9c21
Gruy <mickael.gerard.dev@gmail.com>
2022-05-10 23:15:20 +0200

    add function experience_detail & creer_view

--- app_site_cv/experience/views.py
+++ app_site_cv/experience/views.py
@@ -10,3 +10,17 @@ from django.urls import reverse
 def experience_pro(request):
     experiences = ExperiencePro.objects.all()
     return render(request, 'experience/experience.html', context={'experiences': experiences})
+
+
+def experience_detail(request, slug):
+    experience = get_object_or_404(ExperiencePro, slug=slug)
+    return render(request, 'experience/detail.html', context={"experience": experience})
+
+
+def creer_view(request):
+    form = ExperienceForm
+    if request.method == 'POST':
+        form = ExperienceForm(request.POST, request.FILES)
+        form.save()
+        return HttpResponseRedirect(reverse('experiences:experiences'))
+    return render(request, "experience/creer.html", context={'form': form})
