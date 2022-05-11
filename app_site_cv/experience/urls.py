b7fa00e58d2d3275b5a947ab8abed31898f0e094
Gruy <mickael.gerard.dev@gmail.com>
2022-05-11 14:20:15 +0200

    change function name

--- app_site_cv/experience/urls.py
+++ app_site_cv/experience/urls.py
@@ -7,5 +7,5 @@ app_name = "experiences"
 urlpatterns = [
     path('experience', views.experiences_pro, name='experiences'),
     path('creer', views.creer_view, name='creer'),
-    path('<slug:slug>', views.experience_pro, name='experience')
+    path('<slug:slug>/', views.experience_pro, name='experience')
 ]
