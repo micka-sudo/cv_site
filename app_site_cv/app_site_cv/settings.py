1050f0de17ee56f4f34f0af52a47eea3b2cdb526
Gruy <mickael.gerard.dev@gmail.com>
2022-05-12 20:55:44 +0200

    correct bug

--- app_site_cv/app_site_cv/settings.py
+++ app_site_cv/app_site_cv/settings.py
@@ -143,6 +143,6 @@ ADMIN_EMAIL = ['mickael.gerard.site@gmail.com', ]
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_HOST_USER = 'mickael.gerard.site@gmail.com'
-EMAIL_HOST_PASSWORD = "xxxxx"
+EMAIL_HOST_PASSWORD = os.environ.get("password")
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
