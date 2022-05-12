b4b45b9e77eaa227c473d65ce0cdd68894300c52
Gruy <mickael.gerard.dev@gmail.com>
2022-05-12 09:38:04 +0200

    correct bug

--- app_site_cv/skills/models.py
+++ app_site_cv/skills/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 
 
-class Skills(models, Model):
+class Skills(models.Model):
     titre = models.CharField(max_length=15)
     niveau = models.IntegerField(max_length=3)
-    
