887a1185445996f01842a35a3e57d6268f9284a9
Gruy <mickael.gerard.dev@gmail.com>
2022-05-12 18:09:46 +0200

    make test send email

--- app_site_cv/app_site_cv/settings.py
+++ app_site_cv/app_site_cv/settings.py
@@ -151,6 +151,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # EMAIL_HOST = 'smtp.gmail.com'
 # EMAIL_PORT = '587'
 # EMAIL_HOST_USER = 'mickael.gerard.site@gmail.com'
-# EMAIL_HOST_PASSWORD = '@bobleponge159%'
+# EMAIL_HOST_PASSWORD = '@%'
 # EMAIL_USE_TLS = True
 # EMAIL_USE_SSL = False
