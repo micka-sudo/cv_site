ecb60fb89fd08e6bef5caf4c480f3b728a8b8199
Gruy <mickael.gerard.dev@gmail.com>
2022-05-12 16:24:18 +0200

    change path admin

--- app_site_cv/app_site_cv/urls.py
+++ app_site_cv/app_site_cv/urls.py
@@ -5,8 +5,8 @@ from django.conf import settings
 
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', include('home.urls'), name='home'),
     path('', include('skills.urls'), name='skills'),
     path('', include('experience.urls'), name='experiences'),
-    path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
