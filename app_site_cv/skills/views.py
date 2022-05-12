5f5c1f05b689f01070dbda8d7e88c7fd4ffb08d4
Gruy <mickael.gerard.dev@gmail.com>
2022-05-12 09:57:57 +0200

    add import

--- app_site_cv/skills/views.py
+++ app_site_cv/skills/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .models import Skills
+from .forms import SkillsForm
+from django.urls import reverse
+from django.http import Http404, HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
 
 
 def skills(request):
     return render(request, 'skills/skills.html')
+
+
+def create_skills(request):
+    form =
