6396d0190f24b62d82e34097d27d8318f038fafb
Gruy <mickael.gerard.dev@gmail.com>
2022-05-12 16:27:57 +0200

    add function __str__

--- app_site_cv/skills/models.py
+++ app_site_cv/skills/models.py
@@ -6,3 +6,6 @@ from django.db import models
 class Skills(models.Model):
     titre = models.CharField(max_length=15)
     niveau = models.IntegerField()
+
+    def __str__(self):
+        return self.titre
