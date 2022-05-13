fc41316106b03165459de45260e9fb9867810443
Gruy <mickael.gerard.dev@gmail.com>
2022-05-13 10:41:22 +0200

    test datetime format

--- app_site_cv/app_site_cv/settings.py
+++ app_site_cv/app_site_cv/settings.py
@@ -110,13 +110,16 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_TZ = True
+
+USE_L10N = False
+DATE_INPUT_FORMAT = ["%d %B %Y"]
 
 
 # Static files (CSS, JavaScript, Images)
--- app_site_cv/experience/forms.py
+++ app_site_cv/experience/forms.py
@@ -1,8 +1,16 @@
 from django.forms import ModelForm
 from .models import ExperiencePro
+from django import forms
 
 
 class ExperienceForm(ModelForm):
     class Meta:
         model = ExperiencePro
         fields = ['metier', 'societe', 'contenu', 'slug', 'date_start', 'date_end', 'image']
+
+
+
+
+
+
+
