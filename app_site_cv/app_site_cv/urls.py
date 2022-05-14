ebf5bf288c2b84f9bce64baadb0f0924212ed885
Gruy <mickael.gerard.dev@gmail.com>
2022-05-14 10:11:11 +0200

    clean & rearrange code

--- app_site_cv/app_site_cv/urls.py
+++ app_site_cv/app_site_cv/urls.py
@@ -1,14 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from django.conf.urls.static import static
-from django.conf import settings
-
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('home.urls'), name='home'),
-    path('', include('skills.urls'), name='skills'),
-    path('', include('contact.urls'), name='contact'),
-    path('', include('etudes.urls'), name='etudes'),
-    path('', include('experience.urls'), name='experiences'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('', include('home.urls'), name='home'),
+                  path('', include('skills.urls'), name='skills'),
+                  path('', include('contact.urls'), name='contact'),
+                  path('', include('etudes.urls'), name='etudes'),
+                  path('', include('experience.urls'), name='experiences'),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
