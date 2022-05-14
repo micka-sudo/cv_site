7d9a7ef24e7eabeb2507bcb46750f5e91b992b27
Gruy <mickael.gerard.dev@gmail.com>
2022-05-14 10:19:48 +0200

    clean & rearrange code

--- app_site_cv/experience/forms.py
+++ app_site_cv/experience/forms.py
@@ -1,16 +1,9 @@
 from django.forms import ModelForm
+
 from .models import ExperiencePro
-from django import forms
 
 
 class ExperienceForm(ModelForm):
     class Meta:
         model = ExperiencePro
         fields = ['metier', 'societe', 'contenu', 'slug', 'date_start', 'date_end', 'image']
-
-
-
-
-
-
-
