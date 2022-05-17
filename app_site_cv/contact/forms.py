731b7495979a5cf7f2588be32e4f43583b6812b8
Gruy <mickael.gerard.dev@gmail.com>
2022-05-17 11:16:28 +0200

    clean code

--- app_site_cv/contact/forms.py
+++ app_site_cv/contact/forms.py
@@ -6,12 +6,3 @@ class ContactForm(ModelForm):
     class Meta:
         model = Contact
         fields = '__all__'
-
-
-
-
-# class ContactForm(forms.Form):
-#     nom = forms.CharField(max_length=200)
-#     prenom = forms.CharField(max_length=200)
-#     email = forms.EmailField()
-#     message = forms.CharField(widget=forms.Textarea)
