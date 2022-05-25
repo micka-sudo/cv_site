9eab19187986149d87345fceb595c1334c436706
Gruy <mickael.gerard.dev@gmail.com>
2022-05-25 17:10:15 +0200

    test allowed host

--- app_site_cv/app_site_cv/settings.py
+++ app_site_cv/app_site_cv/settings.py
@@ -32,7 +32,8 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'mickaelgerard.pythonanywhere']
+# ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 # Application definition
